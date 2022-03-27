f165738e71960b9fb5cc6ec8e6caf50c9d8e22b5
Mxxss <89600914+Mxxss@users.noreply.github.com>
2022-03-27 21:23:03 +0200

    Update RIPHUB.lua

--- RIPHUB.lua
+++ RIPHUB.lua
@@ -14,13 +14,19 @@ local gamedata = {
 ["5256165620"] = "http://void-scripts.com/RIP/old/BlastOff.lua";
 ["1862413075"] = "http://void-scripts.com/RIP/old/FarmingSimulator.lua";
 ["6766156863"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/StrongmanSimulator.lua";
-["7009799230"] = "https://raw.githubusercontent.com/vfvo/power-wash-simulator/main/powerwashsim.lua";
+["7009799230"] = "https://raw.githubusercontent.com/vfvo/scripts/main/pressurewashingsim.lua";
 ["5023820864"] = "https://raw.githubusercontent.com/vfvo/trade-tower/main/tradesim.lua";
 ["6490016198"] = "https://raw.githubusercontent.com/vfvo/slayer-tycoon/main/slayertycoon.lua";
 ["8479386309"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/EggHunt2022LostinTime.lua";
 ["6884319169"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MicUp.lua";
 ["340809482"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/LimitedSimulator.lua";
 ["70501379"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/TradeHangout.lua";
+["5736409216"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MallTycoon.lua";
+["8967359816"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/24kGoldnConcertExperience.lua";
+["54865335"] = "https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/UltimateDriving.lua";
+["7860844204"] = "https://raw.githubusercontent.com/vfvo/life-sentence/main/lifesentence.lua";
+["8328351891"] = "https://raw.githubusercontent.com/vfvo/megamansions-tycoon/main/megamansiontycoon.lua";
+["6170705260"] = "https://raw.githubusercontent.com/vfvo/scripts/main/blendingsim2.lua";
 }
 
 for i,v in pairs(gamedata) do
